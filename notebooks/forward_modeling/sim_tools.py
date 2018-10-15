2f52611262c6dafbe1971871127554e18291ea6a
Vince-ec <vestrada78840@gmail.com>
2018-10-14 22:24:19 -0500

    updated sim_tools

--- notebooks/forward_modeling/sim_tools.py
+++ notebooks/forward_modeling/sim_tools.py
@@ -1,6 +1,7 @@
 __author__ = 'Vince.ec'
 
 from grizli import model
+from grizli import multifit
 import numpy as np
 from scipy.interpolate import interp1d
 from astropy.table import Table
@@ -34,14 +35,12 @@ def Extract_BeamCutout(target_id, grism_file, mosaic, seg_map, instruement, cata
 def Scale_model(data, sigma, model):
     return np.sum(((data * model) / sigma ** 2)) / np.sum((model ** 2 / sigma ** 2))
 
-
 class Gen_spec(object):
-    def __init__(self, beam, redshift,gal_wv, gal_fl, gal_er, minwv = 7800, maxwv = 11200):
+    def __init__(self, beam, redshift, spec_file, minwv = 7800, maxwv = 11200):
         self.beam = model.BeamCutout(fits_file = beam)
         self.redshift = redshift
-        self.gal_wv = gal_wv 
-        self.gal_fl = gal_fl
-        self.gal_er = gal_er
+        self.gal_wv, self.gal_fl, self.gal_er = np.load(spec_file)
+        
         """ 
 
 
@@ -49,10 +48,10 @@ class Gen_spec(object):
 
         IDX = [U for U in range(len(self.gal_wv)) if minwv <= self.gal_wv[U] <= maxwv]
 
-        self.gal_wv_rf = gal_wv[IDX] / (1 + self.redshift)
-        self.gal_wv = gal_wv[IDX]
-        self.gal_fl = gal_fl[IDX]
-        self.gal_er = gal_er[IDX]
+        self.gal_wv_rf = self.gal_wv[IDX] / (1 + self.redshift)
+        self.gal_wv = self.gal_wv[IDX]
+        self.gal_fl = self.gal_fl[IDX]
+        self.gal_er = self.gal_er[IDX]
 
         ## Get sensitivity function
         flat = self.beam.flat_flam.reshape(self.beam.beam.sh_beam)
@@ -60,7 +59,9 @@ class Gen_spec(object):
         
         self.filt = interp1d(fwv, ffl)(self.gal_wv)
         
-    def Sim_spec(self, model_wv, model_fl, model_redshift = 0, dust = 0):
+    def Sim_spec(self, model_file, model_redshift = 0, dust = 0):
+        model_wv, model_fl = np.load(model_file)
+        
         if model_redshift ==0:
             model_redshift = self.redshift 
         
@@ -71,6 +72,47 @@ class Gen_spec(object):
         w, f, e = self.beam.beam.optimal_extract(self.beam.model , bin=0)
 
         ifl = interp1d(w, f)(self.gal_wv)
+        
+        C = Scale_model(self.gal_fl, self.gal_er, ifl / self.filt)
+
+        self.fl = C * ifl / self.filt
+        
+class Gen_MB_spec(object):
+    def __init__(self, beams, redshift, spec_file, minwv = 7800, maxwv = 11200):
+        self.mb = multifit.MultiBeam(beams)
+        self.redshift = redshift
+        self.gal_wv, self.gal_fl, self.gal_er = np.load(spec_file)
+        
+        """ 
+
+
+        """
+
+        IDX = [U for U in range(len(self.gal_wv)) if minwv <= self.gal_wv[U] <= maxwv]
+
+        self.gal_wv_rf = self.gal_wv[IDX] / (1 + self.redshift)
+        self.gal_wv = self.gal_wv[IDX]
+        self.gal_fl = self.gal_fl[IDX]
+        self.gal_er = self.gal_er[IDX]
+
+        ## Get sensitivity function
+        flat = self.mb.optimal_extract(self.mb.flat_flam[self.mb.fit_mask])
+        
+        self.filt = interp1d(flat['G102']['wave'], flat['G102']['flux'])(self.gal_wv)
+        
+    def Sim_spec(self, model_file, model_redshift = 0, dust = 0):
+        mwv, mfl = np.load(model_file)
+        
+        if model_redshift ==0:
+            model_redshift = self.redshift 
+        
+        ## Create model
+        spec = self.mb.get_flat_model(spectrum_1d = [mwv *(1 + model_redshift), mfl])
+
+        ## Extract model and flat
+        sp = self.mb.optimal_extract(spec)
+        
+        ifl = interp1d(sp['G102']['wave'], sp['G102']['flux'])(self.gal_wv)
         
         C = Scale_model(self.gal_fl, self.gal_er, ifl / self.filt)
 
